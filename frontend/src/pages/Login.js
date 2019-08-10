314ba5ebfd38469df003e3f4bc4082aecc4da96e
Helena Paixão <hp.helenapaixao@gmail.com>
2019-08-10 16:23:00 -0300

    quase finalizando, verificar o bug do main

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,19 +5,21 @@ import api from '../services/api'
 
 import logo from '../assets/logo.svg'
 
- export default function Login() {
+ export default function Login({history}) {
+
    const [username, setUsername ] = useState('');
 
   async function handleSubmit(e){
     e.preventDefault();
 
-    const response = await api.post('/devs', {
+    //cadastrar o dev
+    const response = await api.post('/devs', 
+    {
       username,
-    })
+    });
 
-    // const {_id} = response.data;
-    // history.push(`/dev/${_id}`)
-
+    const {_id} = response.data;
+    history.push(`/dev/${_id}`)
   }
   return (
     <div className='login-container'>
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,63 +1,46 @@
-import React, {userEffect } from 'react'
+import React, {userEffect, userState } from 'react'
 import '../pages/Main.css'
-
+import api from '../services/api'
 import logo from '../assets/logo.svg'
 import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
 
 export default function Main({match}){
+  const [users, setUsers] = userState([]);
+  userEffect(() => {
+
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: { 
+          user:match.params.id,
+        }
+      })
+      setUsers(response.data)
+    }
+    loadUsers();
+  },[match.params.id])
   return(
     <div className="main-container">
       <img src={logo} alt='Tindev'/>
       <ul>
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt='' />
-          <footer>
-            <strong>Teste</strong>
-            <p>Teste teste</p>
-          </footer>
-
-          <div className="buttons">
-            <button type='button'>
-                <img src={dislike} alt='Dislike'/>
-            </button>
-            <button type='button'>
-                <img src={like} alt='Like'/>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt='' />
-          <footer>
-            <strong>Teste</strong>
-            <p>Teste teste</p>
-          </footer>
-
-          <div className="buttons">
-            <button type='button'>
-                <img src={dislike} alt='Dislike'/>
-            </button>
-            <button type='button'>
-                <img src={like} alt='Like'/>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt='' />
-          <footer>
-            <strong>Teste</strong>
-            <p>Teste teste</p>
-          </footer>
-
-          <div className="buttons">
-            <button type='button'>
-                <img src={dislike} alt='Dislike'/>
-            </button>
-            <button type='button'>
-                <img src={like} alt='Like'/>
-            </button>
-          </div>
-        </li>
+        {users.map(user => (
+           <li>
+           <img src={user.avatar} alt={user.name} />
+           <footer>
+             <strong>{user.name}</strong>
+             <p>{user.bio}</p>
+           </footer>
+ 
+           <div className="buttons">
+             <button type='button'>
+                 <img src={dislike} alt='Dislike'/>
+             </button>
+             <button type='button'>
+                 <img src={like} alt='Like'/>
+             </button>
+           </div>
+         </li>
+        ))}
       </ul>
     </div>
 
